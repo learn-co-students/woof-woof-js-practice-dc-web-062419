49c3c967d732ba0608e561931433642e43fcc86f
Luke O'Malley <Lukeomalley95@gmail.com>
2019-08-09 11:59:18 -0400

    dome

--- src/index.js
+++ src/index.js
@@ -8,15 +8,23 @@ document.addEventListener('DOMContentLoaded', () => {
     .then(data => renderDogBar(data));
 });
 
+function goodBoysOnly(dogs) {
+  return dogs.filter(dog => dog.isGoodDog);
+}
+
 function filterDoggos() {
   fetch('http://localhost:3000/pups')
     .then(res => res.json())
     .then(data => {
       if (isFiltered) {
         renderDogBar(data);
+        document.querySelector('#good-dog-filter').innerText =
+          'Filter good dogs: OFF';
         isFiltered = !isFiltered;
       } else {
-        renderDogBar(data.filter(dog => dog.isGoodDog === true));
+        renderDogBar(data.filter(dog => dog.isGoodDog));
+        document.querySelector('#good-dog-filter').innerText =
+          'Filter good dogs: ON';
         isFiltered = !isFiltered;
       }
     });
@@ -29,38 +37,54 @@ function renderDogBar(dogs) {
     dogButton = document.createElement('button');
     dogButton.innerText = dog.name;
     dogButton.id = dog.id;
-    dogButton.addEventListener('click', renderDogInfo);
+    dogButton.addEventListener('click', e => renderDogInfo(e, dog));
     dogBar.appendChild(dogButton);
   });
 }
 
-function renderDogInfo(e) {
-  dogId = parseInt(e.target.id, 10);
-  fetch(`http://localhost:3000/pups/${dogId}`)
+function renderDogInfo(e, dog) {
+  fetch(`http://localhost:3000/pups/${dog.id}`)
     .then(res => res.json())
     .then(dog => {
       const dogInfo = document.querySelector('#dog-info');
       dogInfo.innerHTML = `
       <img src=${dog.image} alt=${name} />
       <h1>${dog.name}</h1>
-      <p>${dog.isGoodDog}</p>
       `;
       button = dog.isGoodDog
-        ? renderGoodBoyButton(true)
-        : renderGoodBoyButton(false);
+        ? renderGoodBoyButton(dog)
+        : renderGoodBoyButton(dog);
       dogInfo.appendChild(button);
     });
 }
 
-function renderGoodBoyButton(status) {
+function renderGoodBoyButton(dog) {
   const button = document.createElement('button');
-  status
+  dog.isGoodDog
     ? (button.innerText = 'Make Bad Boy')
     : (button.innerText = 'Make Good Boy');
-  button.addEventListener('click', setGoodBoyStatus);
+  button.addEventListener('click', e => setGoodBoyStatus(e, dog));
+  button.id = `dog-${dog.id}`;
   return button;
 }
 
-function setGoodBoyStatus(e) {
-  console.log(e);
+function setGoodBoyStatus(e, dog) {
+  // update the db with the new goodboy status
+  fetch(`http://localhost:3000/pups/${dog.id}`, {
+    method: `PATCH`,
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({ isGoodDog: !dog.isGoodDog }),
+  })
+    .then(res => res.json())
+    .then(dog => {
+      document
+        .querySelector(`#dog-${dog.id}`)
+        .replaceWith(renderGoodBoyButton(dog));
+      fetch('http://localhost:3000/pups')
+        .then(res => res.json())
+        .then(dogs => renderDogBar(goodBoysOnly(dogs)));
+    });
 }
